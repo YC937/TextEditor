e5f75ca384df637d71bde3d1f40d384a8f5b12a1
unknown <ymingc.12@gmail.com>
2023-08-21 15:57:10 -0400

    src-sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Cache name
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
